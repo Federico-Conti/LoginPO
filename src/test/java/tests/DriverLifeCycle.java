14a623fef1058ff7ac3610a662f6665a8c73c6f7
Federico Conti <federicoconti1702@gmail.com>
2026-05-29 00:07:00 +0200

    add arguments

--- src/test/java/tests/DriverLifeCycle.java
+++ src/test/java/tests/DriverLifeCycle.java
@@ -1,23 +1,34 @@
 package tests;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverLifeCycle {
-    
-protected WebDriver driver;
+
+    protected WebDriver driver;
 
     @BeforeEach
     public void setUp(){
         // declaration and instantiation of objects/variables
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
     }
 
     @AfterEach
     public void teardown(){
         // close Chrome
-        driver.close();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
